1dd56a8ab7a728aec84b21b1955a2564e7a18c03
Renan Souza <renan_souza@icloud.com>
2018-07-12 00:38:30 -0300

    Upload File Test Implementation.

---
+++ src/test/java/com/renansouza/processor/file/FileControllerIT.java
@@ -0,0 +1,44 @@
+package com.renansouza.processor.file;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class FileControllerIT {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void shouldSaveUploadedFile() throws Exception {
+        Assert.assertTrue(new File("src/test/resources/Files.zip").exists());
+
+        MockMultipartFile multipartFile = new MockMultipartFile("Files.zip", "Files.zip", "", new FileInputStream(new File("src/test/resources/Files.zip")));
+        System.out.println(multipartFile.getOriginalFilename());
+
+        mvc.perform(MockMvcRequestBuilders.multipart("/upload").file("file", multipartFile.getBytes())
+            .param("env", "1")
+            .param("flow","1"))
+                .andExpect(status().isOk())
+                .andExpect(status().is(200))
+                .andExpect(content().string("Successfully uploaded files."));
+    }
+
+}
